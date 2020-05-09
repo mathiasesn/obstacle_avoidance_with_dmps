4ef188e8d2b2006fb9d12c7e764cd7e8e9fb6d92
Emil Ancker <emil@ancker.com>
2020-05-09 12:20:54 +0200

    DLS - with attraction

--- LinkCollisionAvoidance/avoidanceIK.m
+++ LinkCollisionAvoidance/avoidanceIK.m
@@ -17,10 +17,16 @@ function qd = avoidanceIK(robot, config, obstaclePosition, dxe)
     
    
     % Compute qd
-    %if sum(norm(( eye(7) - pinv(Je)*Je ) * pinv( J0 * ( eye(7) - pinv(Je)*Je ) ))) > 1e6 % Avoid large jumps
-    %    disp('Without null-space movement')
-    %    qd = pinv(Je)*dxe;
-    %else
-    qd = pinv(Je)*dxe + ( eye(7) - pinv(Je, 1e-10)*Je ) * pinv( J0 * ( eye(7) - pinv(Je)*Je ), 1e-10 ) * ( dx0 - J0*pinv(Je,1e-10)*dxe );
-    %end
+    npMove = ( eye(7) - pinv(Je, 1e-10)*Je ) * pinv( J0 * ( eye(7) - pinv(Je)*Je ), 1e-10 ) * ( dx0 - J0*pinv(Je,1e-10)*dxe );
+    %% Pseudo inverse based
+     %qd = pinv(Je)*dxe + ( eye(7) - pinv(Je, 1e-10)*Je ) * pinv( J0 * ( eye(7) - pinv(Je)*Je ), 1e-10 ) * ( dx0 - J0*pinv(Je,1e-10)*dxe );
+    %% Damped least squares
+    lambda = 0.05; % lambda = 0.001
+    if norm(npMove) > 1e3
+        npMove = 0
+    end
+    %qd = Je'*inv(Je*Je' + lambda^2*eye(6))*dxe;% + npMove;
+    
+    qd = Je' * linsolve(Je*Je' + lambda^2*eye(6), dxe) + npMove;
+    
 end
